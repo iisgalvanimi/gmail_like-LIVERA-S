8fc76734fa3a1ac2d7a08bfe4e3c2861d44f5fe4
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-03-18 18:51:22 +0000

    All Done

--- Email-Livera/src/app/sent-mail/sent-mail.component.ts
+++ Email-Livera/src/app/sent-mail/sent-mail.component.ts
@@ -8,11 +8,22 @@ import { Email } from './sentMail.model'; //Non cancellare gli altri import
 })
 export class SentMailComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'card mt-4';
+  @HostBinding('class') cssColor = 'white';
   @Input() mail:Email = undefined!;
+  show = false;
+  Speci =false;
 
   ngOnInit() {}
 
   Espen(): boolean{
+    this.show = !this.show
+    this.Speci = !this.Speci;
     return false;
+  }
+  Spec(){
+    if(this.cssColor == 'white')
+      this.cssColor = 'yellow';
+    else
+      this.cssColor = 'white';
   }
 }
